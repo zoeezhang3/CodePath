e7750c62a3e5be4203da548565eb56a13e44303f
Zoe Zhang <zhangyizhuozoe@gmail.com>
2025-02-27 00:10:05 -0500

    chore: solve problem 3: Gallery Wall

--- session2_unit2.py
+++ session2_unit2.py
@@ -18,9 +18,9 @@ art_pieces1 = [1,3,2,2,5,2,3,7]
 art_pieces2 = [1,2,3,4]
 art_pieces3 = [1,1,1,1]
 
-print(find_balanced_subsequence(art_pieces1))
-print(find_balanced_subsequence(art_pieces2))
-print(find_balanced_subsequence(art_pieces3))
+# print(find_balanced_subsequence(art_pieces1))
+# print(find_balanced_subsequence(art_pieces2))
+# print(find_balanced_subsequence(art_pieces3))
 
 def is_authentic_collection(art_pieces):
   if not art_pieces:
@@ -43,6 +43,26 @@ collection1 = [2, 1, 3]
 collection2 = [1, 3, 3, 2]
 collection3 = [1, 1]
 
-print(is_authentic_collection(collection1))
-print(is_authentic_collection(collection2))
-print(is_authentic_collection(collection3))
+# print(is_authentic_collection(collection1))
+# print(is_authentic_collection(collection2))
+# print(is_authentic_collection(collection3))
+
+def organize_exhibition(collection):
+  count_dict = Counter(collection)
+  two_d_arr = []
+  
+  while any(count_dict.values()):       # the loop will stop when all the values are 0
+    two_d_arr_row = []
+    for key in count_dict:
+      if count_dict[key] > 0:
+        two_d_arr_row.append(key)
+        count_dict[key] -= 1
+    two_d_arr.append(two_d_arr_row)
+      
+  return two_d_arr
+    
+collection1 = ["O'Keefe", "Kahlo", "Picasso", "O'Keefe", "Warhol", "Kahlo", "O'Keefe"]
+collection2 = ["Kusama", "Monet", "Ofili", "Banksy"]
+
+print(organize_exhibition(collection1))
+print(organize_exhibition(collection2))
