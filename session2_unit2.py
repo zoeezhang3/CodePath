db34b2063c81bcfe76e7480e2f229ceab12b5c23
Zoe Zhang <zhangyizhuozoe@gmail.com>
2025-02-26 23:48:35 -0500

    chore: problem 2 for unit 2

--- session2_unit2.py
+++ session2_unit2.py
@@ -18,8 +18,31 @@ art_pieces1 = [1,3,2,2,5,2,3,7]
 art_pieces2 = [1,2,3,4]
 art_pieces3 = [1,1,1,1]
 
-# print(find_balanced_subsequence(art_pieces1))
-# print(find_balanced_subsequence(art_pieces2))
-# print(find_balanced_subsequence(art_pieces3))
+print(find_balanced_subsequence(art_pieces1))
+print(find_balanced_subsequence(art_pieces2))
+print(find_balanced_subsequence(art_pieces3))
 
+def is_authentic_collection(art_pieces):
+  if not art_pieces:
+    return False
+  
+  max_num = max(art_pieces)
+  
+  if len(art_pieces) != max_num+1:
+    return False
+  
+  count = Counter(art_pieces)
+  
+  for i in range(1, max_num):
+    if count[i] != 1:
+      return False
+  
+  return count[max_num] == 2
 
+collection1 = [2, 1, 3]
+collection2 = [1, 3, 3, 2]
+collection3 = [1, 1]
+
+print(is_authentic_collection(collection1))
+print(is_authentic_collection(collection2))
+print(is_authentic_collection(collection3))
