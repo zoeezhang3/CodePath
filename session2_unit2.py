49370e8096d7fb3f8f34a0607536254b45a106a6
Zoe Zhang <zhangyizhuozoe@gmail.com>
2025-02-27 23:49:22 -0500

    chore: problem 6

--- session2_unit2.py
+++ session2_unit2.py
@@ -113,5 +113,26 @@ def beauty_sum(collection):
       
     return total_beauty 
 
-print(beauty_sum("aabcb")) 
-print(beauty_sum("aabcbaa"))
+# print(beauty_sum("aabcb")) 
+# print(beauty_sum("aabcbaa"))
+
+# Problem 6: Counting Divisible Collections in the Gallery
+def count_divisible_collections(collection_sizes, k):
+    count = 0
+    
+    for i in range(len(collection_sizes)):
+      total = 0
+      for j in range(i, len(collection_sizes)):
+        total += collection_sizes[j]
+        if total%k == 0:
+          count+=1
+          
+    return count
+
+nums1 = [4, 5, 0, -2, -3, 1]
+k1 = 5
+nums2 = [5]
+k2 = 9
+
+print(count_divisible_collections(nums1, k1))  
+print(count_divisible_collections(nums2, k2)) 
